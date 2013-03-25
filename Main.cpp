3ed1fd7468df349fd92b40aa8586c35f10da37ae
Hamish-Bassett <hamish.bassett89@gmail.com>
2013-03-25 17:23:54 +1100

    base program

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 25/03/2013
+ *      Author: hamish
+ */
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	cout<<"Hello World"<<endl;
+
+	return 0;
+}
+
+
